5b20b9cfdbc240799727a3bbfaf8cf5a8935cc2d
ShadowConYT <iamajay6122@gmail.com>
2024-01-11 22:16:34 +0530

    Sheesh I can do Java

---
+++ Java/LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch {
+
+    public static void main(String[] args){
+        int nums[] = {0,6,1,9};
+        int target = 6;
+
+        int result = linear(nums, target);
+        
+        if (result != -1)
+            System.out.println("The Element found in the Index: " + result);
+        else
+            System.out.println("The Element not found");
+    };
+
+    public static int linear(int[] nums, int target){
+        
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == target)
+                return i;
+        }
+
+        return -1;
+    }
+
+};
