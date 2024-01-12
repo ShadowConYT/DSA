0d47f0aef237b688baabd84a867755423d31ce4a
ShadowConYT <iamajay6122@gmail.com>
2024-01-12 13:27:58 +0530

    Binary Recursion Added as well

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
         
         int target = 1;
 
-        int result = binary(nums, target);
+        int result = binaryRecursion(nums, target, 0, nums.length-1);
 
         if (result != -1)
             System.out.println("The Element found in the Index: " + result);
@@ -37,5 +37,26 @@ public class BinarySearch {
 
         return -1;
 
+    }
+
+    // binary recursion
+
+    public static int binaryRecursion(int[] nums, int target, int low, int high){
+        Arrays.sort(nums);
+        if (low <= high){
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+            // [1,2,3,4,5,6]
+            else if (nums[mid] < target){
+                return binaryRecursion(nums, target, mid+1, high);
+            }
+            else{
+                return binaryRecursion(nums, target, low, mid-1);
+            }
+        }
+        return -1;
     }
 }
